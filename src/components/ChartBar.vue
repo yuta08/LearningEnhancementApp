8321c04359992ef5b3e49ec054a911669740ed6f
Ayako-02 <89629680+Ayako-02@users.noreply.github.com>
2023-06-01 10:01:23 +0900

    add chart files

---
+++ src/components/ChartBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <canvas ref="chart"></canvas>
+  </div>
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs';
+
+export default {
+  extends: Bar,
+  mounted() {
+    this.renderChart({
+      labels: ['データ1', 'データ2', 'データ3'],
+      datasets: [
+        {
+          label: 'サンプルデータ',
+          backgroundColor: '#f87979',
+          data: [10, 20, 30]
+        }
+      ]
+    },
+    { responsive: true, maintainAspectRatio: false }
+    );
+  }
+};
+</script>
+
+<style>
+/* グラフのスタイルを指定する必要があれば追加します */
+</style>
