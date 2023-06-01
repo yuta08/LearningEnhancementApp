f99401fffeb4ccb37dedd47c4d6560343292e20a
Ayako-02 <89629680+Ayako-02@users.noreply.github.com>
2023-06-01 10:02:30 +0900

    Update LearningReview.vue

--- src/views/LearningReview.vue
+++ src/views/LearningReview.vue
@@ -2,7 +2,9 @@
     <v-app class="home">
       <SideBar />
       <v-main>
-        
+        <div>
+          <BarChart />
+        </div>
   
       </v-main>
     </v-app>
@@ -21,17 +23,18 @@
   <script>
     import SideBar from "@/components/SideBar.vue";
     // import firebase from "@/firebase/firebase"
+    import BarChart from '@/components/ChartBar.vue';
   
     export default {
       
       data: () => ({
         
       }),
-      components: { SideBar },
+      components: { SideBar ,BarChart,},
       methods: {
         
         
       }
-    }
+    };
   </script>
   
